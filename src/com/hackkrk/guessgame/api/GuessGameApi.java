4e5113d51cd019cca771e346ee58ff8817538c74
Bartosz Filipowicz <bartek.f@applicake.com>
2012-06-21 19:06:13 +0200

    adds http root address

--- src/com/hackkrk/guessgame/api/GuessGameApi.java
+++ src/com/hackkrk/guessgame/api/GuessGameApi.java
@@ -5,9 +5,13 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 public class GuessGameApi {
 
+  private static String API_ROOT_HTTP = "http://hackkrk-guess.herokuapp.com";
+
   HttpClient getHttpClient() {
     HttpClient client = new DefaultHttpClient();
     return client;
   }
+  
+  
 
 }
