e130b87d8ffa069c93e3fd31222d01bd87b0ca3d
Bartek Filipowicz <bartek.f@applicake.com>
2012-06-21 18:39:19 +0200

    initial commit

---
+++ gen/com/hackkrk/guessgame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hackkrk.guessgame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/hackkrk/guessgame/GuessGameClientActivity.java
@@ -0,0 +1,13 @@
+package com.hackkrk.guessgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuessGameClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
