f7cc0757f8bb9b94ed42394eaf50afcce8484284
Bartosz Filipowicz <bartek.f@applicake.com>
2012-06-21 20:13:09 +0200

    adds json conversion methods to User class

--- src/com/hackkrk/guessgame/model/User.java
+++ src/com/hackkrk/guessgame/model/User.java
@@ -1,16 +1,25 @@
 package com.hackkrk.guessgame.model;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User implements ConvertibleToJson {
 
   public String username;
   public String token;
+  public String password;
 
   @Override
   public JSONObject toJson() {
-    // TODO Auto-generated method stub
-    return null;
+    JSONObject jsonObject = new JSONObject();
+    try {
+      jsonObject.put("username", username);
+      jsonObject.put("password", password);
+    } catch (JSONException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return jsonObject;
   }
 
 }
