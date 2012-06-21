5186dc207900a918d71b5d8aef75526f830baffe
Bartosz Filipowicz <bartek.f@applicake.com>
2012-06-21 18:58:06 +0200

    adds getHttpClient method to api

--- src/com/hackkrk/guessgame/api/GuessGameApi.java
+++ src/com/hackkrk/guessgame/api/GuessGameApi.java
@@ -1,7 +1,13 @@
 package com.hackkrk.guessgame.api;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class GuessGameApi {
-  
-  
+
+  HttpClient getHttpClient() {
+    HttpClient client = new DefaultHttpClient();
+    return client;
+  }
 
 }
